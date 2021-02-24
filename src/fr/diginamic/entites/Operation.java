49132e1311299f4a7cd6285bbf935e9e37e9c7d6
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-02-24 12:23:09 +0100

    add Operation

--- src/fr/diginamic/entites/Operation.java
+++ src/fr/diginamic/entites/Operation.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Operation {
@@ -21,6 +23,8 @@ public class Operation {
 	private double montant;
 	@Column(name = "motif")
 	private String motif;
-//	@ManyToOne
+	@ManyToOne
+	@JoinColumn(name = "id_compte")
+	private Compte compte;
 
 }
